114e99ba7715ac5cfa8a4633c9a90f982324123c
ridish05 <kollir@tcd.ie>
2022-04-07 23:51:21 +0100

    first submission
    Vancouver Bus App first submission

---
+++ VancouverBusAPP.java
@@ -0,0 +1,9 @@
+/**
+ * VancouverBusApp
+ */
+public class VancouverBusAPP {
+    public static void main(String[] args) {
+        System.out.println("First subbmission of VancouverBusApp");
+    }
+     
+ }
